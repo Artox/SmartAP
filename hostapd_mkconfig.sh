32fe49566381a28ab1b676c117785e5e488d5838
Josua Mayer <privacy@not.given>
2013-02-14 20:56:42 +0100

    Config File Name can now be given as argument

--- hostapd_mkconfig.sh
+++ hostapd_mkconfig.sh
@@ -1,38 +1,44 @@
 #!/bin/sh
 
-###################################
+#################################################################################
 #
 # For now this generates a config file for hostapd
 # Encryption is WPA2 with preshared Key
-# Arguments taken:
-# -s <ssid>
-# -p <preshared key>
 #
-###################################
+#################################################################################
+# USAGE: ./hostapd_mkconfig.sh [flags] args
+# flags:
+#   -s,--ssid:  SSID to use for the Access Point (default: 'SmartAP')
+#   -p,--password:  Password to use for the Access Point (default: 'SuperSecret')
+#   -d,--device:  Wireless LAN Device Name (default: 'wlan0')
+#   -c,--config:  Output File Name (default: 'hostapd.conf')
+#   -h,--[no]help:  show this help (default: false)
+#################################################################################
 
 source ./shflags
 
 DEFINE_string 'ssid' 'SmartAP' 'SSID to use for the Access Point' 's'
 DEFINE_string 'password' 'SuperSecret' 'Password to use for the Access Point' 'p'
 DEFINE_string 'device' 'wlan0' 'Wireless LAN Device Name' 'd'
+DEFINE_string 'config' 'hostapd.conf' 'Output File Name' 'c'
 
 FLAGS "$@" || exit 1
 eval set -- "${FLAGS_ARGV}"
 
-echo "Device: ${FLAGS_device}"
-echo "SSID: ${FLAGS_ssid}"
+device=${FLAGS_device}
+ssid="${FLAGS_ssid}"
+psk="${FLAGS_password}"
+configfile="${FLAGS_config}"
+echo "Device: $device"
+echo "SSID: $ssid"
 echo "Encryption: WPA2"
 echo "Authentication: PresharedKey"
 echo "Channel: 1"
 echo "Mode: 802.11g"
-echo "Password: ${FLAGS_password}"
-
-device=${FLAGS_device}
-ssid=${FLAGS_ssid}
-psk=${FLAGS_password}
+echo "Password: $psk"
 
 # a basic config file is very straight-forward
-cat > hostapd.conf << "EOF"
+cat > "$configfile" << "EOF"
 interface=AUTOREPLACE_DEVICE
 driver=nl80211
 ssid=AUTOREPLACE_SSID
@@ -46,6 +52,6 @@ EOF
 sed -e "s;AUTOREPLACE_DEVICE;$device;g" \
     -e "s;AUTOREPLACE_SSID;$ssid;g" \
     -e "s;AUTOREPLACE_PSK;$psk;g" \
-    -i hostapd.conf
+    -i "$configfile"
 
-echo "Finished Generating hostapd.conf"
+echo "Configuration written to $configfile"
--- udhcpd_mkconfig.sh
+++ udhcpd_mkconfig.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+#######################################################################
+# USAGE: ./udhcpd_mkconfig.sh [flags] args
+# flags:
+#   -s,--start:  First Ip Address to be served (default: '192.168.0.2')
+#   -e,--end:  Last IP to be served (default: '192.168.0.254')
+#   -n,--netmask:  Subnet Mask (default: '255.255.255.0')
+#   -l,--lease:  Lease Time for IPs (default: '3600')
+#   -d,--device:  Wireless LAN Device Name (default: 'wlan0')
+#   -c,--config:  Output File Name (default: 'udhcpd.conf')
+#   -h,--[no]help:  show this help (default: false)
+#######################################################################
+
 source ./shflags
 
 DEFINE_string 'start' '192.168.0.2' 'First Ip Address to be served' 's'
@@ -7,22 +19,24 @@ DEFINE_string 'end' '192.168.0.254' 'Last IP to be served' 'e'
 DEFINE_string 'netmask' '255.255.255.0' 'Subnet Mask' 'n'
 DEFINE_string 'lease' '3600' 'Lease Time for IPs' 'l'
 DEFINE_string 'device' 'wlan0' 'Wireless LAN Device Name' 'd'
+DEFINE_string 'config' 'udhcpd.conf' 'Output File Name' 'c'
 
 FLAGS "$@" || exit 1
 eval set -- "${FLAGS_ARGV}"
 
-echo "First IP: ${FLAGS_start}"
-echo "Last IP: ${FLAGS_end}"
-echo "Netmask: ${FLAGS_netmask}"
-echo "Lease Time: ${FLAGS_lease}"
-echo "Device: ${FLAGS_device}"
 firstip=${FLAGS_start}
 lastip=${FLAGS_end}
 netmask=${FLAGS_netmask}
 leasetime=${FLAGS_lease}
 device=${FLAGS_device}
+configfile="${FLAGS_config}"
+echo "First IP: $firstip"
+echo "Last IP: $lastip"
+echo "Netmask: $netmask"
+echo "Lease Time: $leasetime"
+echo "Device: $device"
 
-cat > udhcpd.conf << "EOF"
+cat > "$configfile" << "EOF"
 start AUTOREPLACE_FISTIP
 end   AUTOREPLACE_LASTIP
 interface AUTOREPLACE_DEVICE
@@ -37,6 +51,6 @@ sed -e "s;AUTOREPLACE_FIRSTIP;$firstip;g" \
     -e "s;AUTOREPLACE_SUBNET;$netmask;g" \
     -e "s;AUTOREPLACE_LEASETIME;$leasetime;g" \
     -e "s;AUTOREPLACE_DEVICE;$device;g" \
-    -i udhcpd.conf
+    -i "$configfile"
 
-echo "Finished Generating udhcpd.conf"
+echo "Configuration written to $configfile"
